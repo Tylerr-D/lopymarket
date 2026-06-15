c0fce63ed25747346c47978f5943dc1386f08c72
Tylerr-D <tylermishra76@gmail.com>
2026-06-16 00:36:14 +0530

    Initial backend setup

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userScheama = new mongoose.Schema({
+
+    username: {
+        type:String,
+        required:true
+    },
+
+     email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    balance: {
+        type: Number,
+        default: 1000
+    }
+},{
+timestamps:true
+}
+);
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,28 @@
+// basic imports :)
+
+require("dotenv").config();
+const express = require("express")
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+connectDB();
+
+const app = express();
+app.use("/api/auth", authRoutes);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res) =>{
+        res.send("Lopymarket API workin");
+
+});
+
+//this is what seniors do hehehe
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
