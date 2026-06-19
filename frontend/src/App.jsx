1e65f5d0dbe2b9d6b507b6d218bc3a6169a0daf2
Tylerr-D <tylermishra76@gmail.com>
2026-06-19 22:55:36 +0530

    added coins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Home from "./pages/Home";
 import MarketPage from "./pages/MarketPage";
 // import Portfolio from "./pages/Portfolio";
+
 
 function App() {
 
   const [page,setPage] = useState("home");
       const [selectedMarketId, setSelectedMarketId] = useState(null);
+      const [user, setUser] = useState(null);
 
-const userId = "6a33c575b0e5e52c34ac85fe";
+
+const userId = "6a357a42670337823e748518";
+
+    async function loadUser() {
+        const res = await axios.get(`http://localhost:5000/api/users/${userId}`);
+        setUser(res.data);
+    }
 
 function openMarket(id){
   setSelectedMarketId(id);
   setPage("market");
 }
 
+useEffect(() => { loadUser(); }, []);
+
 return (
           <div className="container">
 
-            <h1>Lopymarket</h1>
+            <div className="topbar">
+                          <h1>Lopymarket</h1>
+
+                <div className="coins">
+                    Coins: {user ? user.coins.toFixed(2) : "Loading..."}
+                </div>
+            </div>
 
                         <div className="nav">
                 <button className = {page === "home" ? "active":""}
                   onClick={()=> setPage("home")}>Markets</button >
 
-                  
-                                    {/* <button className = {page === "portfolio" ? "active":""}
-onClick ={() => setPage("portfolio")}>Portfolio</button> */}
 
               </div>
             {page === "home" && <Home onMarketClick={openMarket} />}
 
             {page === "market" && <MarketPage marketId ={selectedMarketId} userId={userId} onBack = {() => setPage("home")} />}
 
-              {/* {page === "portfolio" && <Portfolio userId={userId} />} */}
             </div>
 )
 
